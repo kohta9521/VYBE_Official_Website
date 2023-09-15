8cf4864bcc57b4d71906d0e6ecc287882775876b
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-16 05:56:54 +0900

    add new templates components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './styles/App.css'
+import MainLayout from './components/templates/MainLayout'
 
 function App() {
-  return <div className="App">sample</div>
+  return <div className="App"><MainLayout /></div>
 }
 
 export default App
---
+++ src/components/templates/MainLayout.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+// css import
+import "./styles/MainLayout.css";
+
+// components import
+
+
+const MainLayout: React.FC = () => {
+  return (
+    <div className='main'>
+      hello
+    </div>
+  )
+}
+
+export default MainLayout;
