82cfa2bd746a50bb03c064ba3b82490a49f6c68f
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-16 06:42:58 +0900

    add mission components

---
+++ src/components/organisms/Misson.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// css import
+import './styles/Mission.css'
+
+const Mission = () => {
+  return (
+    <div className="mittion">
+      <h1>Mission title</h1>
+    </div>
+  )
+}
+
+export default Mission
