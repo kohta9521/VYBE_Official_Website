6602c0c3ae4719310061557556ee8d4d4a6ca125
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-16 11:56:12 +0900

    error message FC components not use error complete

--- src/components/pages/MainLayout.tsx
+++ src/components/pages/MainLayout.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 // css import
 import './styles/MainLayout.css'
 import Toppage from '../organisms/Toppage'
-import Mission from '../organisms/Misson'
-import Section1 from '../organisms/Section1'
+import Section1 from '../organisms/Sectiogitn1'
 
 const MainLayout = () => {
   return (
     <div className="main">
       <Toppage />
-      {/* <Mission /> */}
       <Section1 />
     </div>
   )
